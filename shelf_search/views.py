97cfba928f341e6fcdba88f5a28175f93000a044
xsan777 <1315969544@qq.com>
2018-10-25 10:25:55 +0800

    V 0.0.3 实现查询粒度精确到店铺

--- shelf_search/views.py
+++ shelf_search/views.py
@@ -44,6 +44,9 @@ def index(request):
     style_coding_list = []
     err = ''
     batchs = ''
+    style = ''
+    shops_list = ''
+    shops = ''
     if request.method == 'POST':
         batchs = request.POST.get('batch')
     # 判断款式号是否有误
@@ -64,46 +67,98 @@ def index(request):
                 spuid_dic['spuid'] = spuids.spuid
                 spuid_dic['brand'] = spuids.brand
                 spuid_list.append(spuid_dic)
-
+            # 款式列表
+            style_coding_data_2 = SpuidComparison.objects.filter(batch=batchs).values('style_coding').distinct().all()
+            style_coding_list = [i['style_coding'] for i in style_coding_data_2]
+            # 店铺列表
+            shops_data = SpuidComparison.objects.filter(batch=batchs, style_coding=style).all()
+            # shops_list = [i.brand for i in shops_data]
+            shops_list = []
+            for i in shops_data :
+                tmp ={}
+                tmp['brand'] = i.brand
+                tmp['spuid'] = i.spuid
+                shops_list.append(tmp)
+            shops= request.POST.get('shopname')
             # 判断该款式是否存在spuid号
             if len(spuid_list) > 0:
                 # 查询data并生成图表
                 over_list = []
-                for spuidss in spuid_list:
-                    data = StoreDailyData.objects.filter(spuid=spuidss['spuid']).values('uv', 'conversion_rate_of_order_payment',
-                                                                                        'conversion_rate_of_payment',
-                                                                                        'number_of_additional_purchases', 'collection_number',
-                                                                                        'number_of_order_items', 'date').all()
-                    x_list = [i['date'] for i in data]
-                    y_list_1 = ['%.2f' % (i['conversion_rate_of_payment'] * 100) for i in data]
-                    y_list_1_2 = []
-                    for i in data:
-                        if i['uv'] == 0:
-                            tmp = '0.00'
-                        else:
-                            tmp = '%.2f' % (i['collection_number'] / i['uv'] * 100)
-                        y_list_1_2.append(tmp)
-                    y_list_1_3 = []
-                    for i in data:
-                        if i['uv'] == 0:
-                            tmp = '0.00'
-                        else:
-                            tmp = '%.2f' % (i['number_of_additional_purchases'] / i['uv'] * 100)
-                        y_list_1_3.append(tmp)
-                    overlap = Overlap()
-                    line = Line()
-                    line.add('转化率', x_list, y_list_1, yaxis_formatter='%', is_smooth=True)
-                    line.add('收藏率', x_list, y_list_1_2, yaxis_formatter='%', is_smooth=True)
-                    line.add('加购率', x_list, y_list_1_3, yaxis_formatter='%', is_smooth=True)
-                    line_2 = Line(spuidss['brand'] + '-' + spuidss['spuid'])
-                    y_list_2 = [i['uv'] for i in data]
-                    y_list_2_2 = [i['number_of_order_items'] for i in data]
-                    line_2.add('UV', x_list, y_list_2, is_smooth=True)
-                    line_2.add('成交量', x_list, y_list_2_2, is_smooth=True)
-                    overlap.add(line_2)
-                    overlap.add(line, is_add_yaxis=True, yaxis_index=1)
-                    ds = overlap.render_embed()
-                    over_list.append(ds)
+                if shops == '全部':
+                    for shop in shops_list:
+                        data = StoreDailyData.objects.filter(spuid=shop['spuid'],brand=shop['brand']).values('uv', 'conversion_rate_of_order_payment',
+                                                                                            'conversion_rate_of_payment',
+                                                                                            'number_of_additional_purchases', 'collection_number',
+                                                                                            'number_of_order_items', 'date').all()
+                        x_list = [i['date'] for i in data]
+                        y_list_1 = ['%.2f' % (i['conversion_rate_of_payment'] * 100) for i in data]
+                        y_list_1_2 = []
+                        for i in data:
+                            if i['uv'] == 0:
+                                tmp = '0.00'
+                            else:
+                                tmp = '%.2f' % (i['collection_number'] / i['uv'] * 100)
+                            y_list_1_2.append(tmp)
+                        y_list_1_3 = []
+                        for i in data:
+                            if i['uv'] == 0:
+                                tmp = '0.00'
+                            else:
+                                tmp = '%.2f' % (i['number_of_additional_purchases'] / i['uv'] * 100)
+                            y_list_1_3.append(tmp)
+                        overlap = Overlap()
+                        line = Line()
+                        line.add('转化率', x_list, y_list_1, yaxis_formatter='%', is_smooth=True)
+                        line.add('收藏率', x_list, y_list_1_2, yaxis_formatter='%', is_smooth=True)
+                        line.add('加购率', x_list, y_list_1_3, yaxis_formatter='%', is_smooth=True)
+                        line_2 = Line(shop['brand'] + '-' + shop['spuid'])
+                        y_list_2 = [i['uv'] for i in data]
+                        y_list_2_2 = [i['number_of_order_items'] for i in data]
+                        line_2.add('UV', x_list, y_list_2, is_smooth=True)
+                        line_2.add('成交量', x_list, y_list_2_2, is_smooth=True)
+                        overlap.add(line_2)
+                        overlap.add(line, is_add_yaxis=True, yaxis_index=1)
+                        ds = overlap.render_embed()
+                        over_list.append(ds)
+                else:
+                    spuid_all = SpuidComparison.objects.filter(batch=batchs,style_coding=style,brand=shops).all()
+                    spuid_list = [i.spuid for i in spuid_all]
+                    for spuid_ in spuid_list:
+                        data = StoreDailyData.objects.filter(spuid=spuid_,).values('uv', 'conversion_rate_of_order_payment',
+                                                                                                              'conversion_rate_of_payment',
+                                                                                                              'number_of_additional_purchases',
+                                                                                                              'collection_number',
+                                                                                                              'number_of_order_items', 'date').all()
+                        x_list = [i['date'] for i in data]
+                        y_list_1 = ['%.2f' % (i['conversion_rate_of_payment'] * 100) for i in data]
+                        y_list_1_2 = []
+                        for i in data:
+                            if i['uv'] == 0:
+                                tmp = '0.00'
+                            else:
+                                tmp = '%.2f' % (i['collection_number'] / i['uv'] * 100)
+                            y_list_1_2.append(tmp)
+                        y_list_1_3 = []
+                        for i in data:
+                            if i['uv'] == 0:
+                                tmp = '0.00'
+                            else:
+                                tmp = '%.2f' % (i['number_of_additional_purchases'] / i['uv'] * 100)
+                            y_list_1_3.append(tmp)
+                        overlap = Overlap()
+                        line = Line()
+                        line.add('转化率', x_list, y_list_1, yaxis_formatter='%', is_smooth=True)
+                        line.add('收藏率', x_list, y_list_1_2, yaxis_formatter='%', is_smooth=True)
+                        line.add('加购率', x_list, y_list_1_3, yaxis_formatter='%', is_smooth=True)
+                        line_2 = Line(shops + '-' + spuid_)
+                        y_list_2 = [i['uv'] for i in data]
+                        y_list_2_2 = [i['number_of_order_items'] for i in data]
+                        line_2.add('UV', x_list, y_list_2, is_smooth=True)
+                        line_2.add('成交量', x_list, y_list_2_2, is_smooth=True)
+                        overlap.add(line_2)
+                        overlap.add(line, is_add_yaxis=True, yaxis_index=1)
+                        ds = overlap.render_embed()
+                        over_list.append(ds)
             else:
                 err = '该款式没有对应的spuid'
         else:
@@ -112,7 +167,8 @@ def index(request):
     #     err = '批次号有误'
     batch_form = Batch()
     return render(request, 'index.html',
-                  {'style_coding': style_coding_list, "line_list": over_list, 'err': err, 'batch_form': batch_form, 'batch': batchs})
+                  {'style_coding': style_coding_list, "line_list": over_list, 'err': err, 'batch_form': batch_form, 'batch': batchs, 'style': style,
+                   'style_coding_list': style_coding_list, 'shops_list': shops_list,'shops':shops})
 
 
 # 动态验证批次号并加载对应的款式号
